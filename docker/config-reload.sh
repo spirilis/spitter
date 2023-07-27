a1b95f0c0207a70cc03dcd23d7630714e5d71108
Eric Brundick <ebrundick@penguinrandomhouse.com>
2023-07-27 15:50:59 -0400

    sanity checking for the config-reload script

--- docker/config-reload.sh
+++ docker/config-reload.sh
@@ -13,16 +13,28 @@ TRIGGERFILE="$2"
 
 function sum_files() {
     cd $WATCH_DIR
-    cat * | sha256sum | awk '{print $1}'
+    # intentionally ignoring any dot-files here
+    cat * 2>/dev/null | sha256sum | awk '{print $1}'
 }
 
 CHECKSUM="$(sum_files)"
+
+# $immediate_reloads is a sanity check to make sure we're not in a loop continuously requesting a reload
+immediate_reloads="0"
 
 while [ 1 ]; do
     sleep 5
     NEWSUM="$(sum_files)"
     if [ "$NEWSUM" != "$CHECKSUM" ]; then
+        immediate_reloads="$(expr $immediate_reloads + 1)"
         touch "$TRIGGERFILE"
         CHECKSUM="$NEWSUM"
+    else
+        immediate_reloads="0"
+    fi
+
+    if [ "$immediate_reloads" -gt "3" ]; then
+        echo "Something's up - we're in a continual reload request loop.  Exit 1 to crash the container"
+        exit 1
     fi
 done
